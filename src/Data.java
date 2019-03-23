62297eb455cef731a76fb14a11c2460f0eecd19b
Ashmeet Singh <ashu6811singh@gmail.com>
2019-03-23 09:52:42 -0400

    vertical order traversal Top view printing

---
+++ src/Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public Integer max;
+    public Integer min;
+    public Integer last;
+
+    Data()
+    {
+        max =0;
+        min = 0;
+
+        last =0;
+    }
+}
