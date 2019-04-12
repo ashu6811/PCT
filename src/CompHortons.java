6692a8e18d9fcecd89f11d85b60b2e721517be80
Ashmeet Singh <ashu6811singh@gmail.com>
2019-04-12 18:30:18 -0400

    CompHortons Basic and advanced done

--- src/CompHortons.java
+++ src/CompHortons.java
@@ -1,7 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class CompHortons {
     public Integer limit1, limit2, limit3;
+    public String[] result;
 
     public static void main(String [] args)
     {
@@ -15,6 +20,8 @@ public class CompHortons {
 
         Integer numOfCustomers;
         numOfCustomers = input.nextInt();
+
+        obj.result = new String[numOfCustomers];
 
         String[] names= new String[numOfCustomers];
         Integer[] ages = new Integer[numOfCustomers];
@@ -32,11 +39,23 @@ public class CompHortons {
 
         // input done...........
 
+        //Result for Basic
         for (int i=0; i<numOfCustomers; i++)
         {
-            obj.printBasicOutput(obj, names[i], ages[i], heightsFeet[i], heightsInches[i]);
+            obj.printBasicOutput(obj, names[i], ages[i], heightsFeet[i], heightsInches[i], i);
             System.out.println();
         }
+
+        // Result for Advanced
+        List<String> resultToSort = new ArrayList<>();
+        resultToSort = Arrays.asList(obj.result);
+        resultToSort.sort(String::compareTo);
+        System.out.println();
+
+        for (int i =0; i < numOfCustomers; i++)
+            System.out.println(resultToSort.toArray()[i]);
+
+
     }
 
     public  Float calcSize (Integer hFeet, Integer hInch)
@@ -50,25 +69,47 @@ public class CompHortons {
 
         return units;
     }
-    public void printBasicOutput(CompHortons obj, String name, Integer age, Integer hFeet, Integer hInch)
+    public void printBasicOutput(CompHortons obj, String name, Integer age, Integer hFeet, Integer hInch, Integer i)
     {
         System.out.print(name);
+        result[i] = "";
+        result[i] += name;
 
         if(age < 18 )
+        {
             System.out.print(" Water");
+            result[i] += " Water";
+        }
         else
+            {
             System.out.print(" Beer");
+            result[i] += " Beer";
+            }
 
         if(0 < calcSize(hFeet, hInch) && calcSize(hFeet, hInch)<= obj.limit1)
+        {
             System.out.print(" ExtraSmall");
+            result[i] += " ExtraSmall";
+        }
         else if(obj.limit1 < calcSize(hFeet, hInch) && calcSize(hFeet, hInch)<= obj.limit2)
+        {
             System.out.print(" Small");
+            result[i] += " Small";
+        }
+
         else if(obj.limit2 < calcSize(hFeet, hInch) && calcSize(hFeet, hInch)<= obj.limit3)
+        {
             System.out.print(" Medium");
+            result[i] += " Medium";
+        }
         else
+            {
             System.out.print(" Large");
+            result[i] += " Large";
+        }
 
         System.out.print(" " + age +" " + hFeet + " " + hInch);
+        result[i] += " " + age +" " + hFeet + " " + hInch;
 
     }
 }
@@ -76,6 +117,12 @@ public class CompHortons {
 25 50 75
 3
 Charles 29 5 0
+Adam 56 6 3
+Bob 11 4 6
+
+25 50 75
+3
+Bob 29 5 0
 Adam 56 6 3
 Bob 11 4 6
 
